d9f98bd5e2852c26f05258d7b6d8c06a44592024
ammar <ammarakram860@hotmail.com>
2023-03-15 00:58:32 +0500

    changes

--- src/components/Form/UpdatePropertyMediaModal.vue
+++ src/components/Form/UpdatePropertyMediaModal.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <b-modal id="editMediaModal" ref="editPropertyMedia" title="Edit Property Media" modal-class="modal-right">
+    <b-modal
+      id="editMediaModal"
+      ref="editPropertyMedia"
+      title="Edit Property Media"
+      modal-class="modal-right"
+    >
       <b-form class="av-tooltip tooltip-label-bottom">
         <b-form-group>
           <b-form-checkbox v-model="isMain" @change="clearAll">
@@ -8,8 +13,17 @@
           </b-form-checkbox>
         </b-form-group>
         <b-form-group label="Drop Image or Document Here">
-          <b-form-file v-if="isMain" v-model="file" ref="file-input"></b-form-file>
-          <b-form-file v-else multiple v-model="files" ref="file-input"></b-form-file>
+          <b-form-file
+            v-if="isMain"
+            v-model="file"
+            ref="file-input"
+          ></b-form-file>
+          <b-form-file
+            v-else
+            multiple
+            v-model="files"
+            ref="file-input"
+          ></b-form-file>
         </b-form-group>
 
         <b-form-group label="Image Position">
@@ -19,31 +33,46 @@
             </b-col>
           </b-row>
         </b-form-group>
-        <b-button class="mb-1" variant="primary" @click="addMedia">ADD</b-button>
+        <b-button class="mb-1" variant="primary" @click="addMedia"
+          >ADD</b-button
+        >
       </b-form>
       <div v-if="listImages.length > 0">
         <h6>Uploaded Images and Documents</h6>
-        <div v-for="(image, index) in listImages" :key="index">
-          <b-row no-gutters>
-            <b-col md="5">
-              <b-card class="m-3" :img-src="image.url" img-left no-body />
-            </b-col>
-            <b-col md="7">
-              <b-card-body>
 
-                <b-row>
-                  <span>Upload Time :</span>
-                  <span>{{ timeFormat(image.uploadTime) }}</span>
-                </b-row>
-                <b-row>
-                  <span>IsMain : </span>
-                  <span><b-form-checkbox disabled v-model="image.isMain" /></span>
-                </b-row>
-                <b-row>
-                  <span>IsActive : </span>
-                  <b-form-checkbox disabled v-model="image.isActive" text-field="IsActive" />
-                </b-row>
-                <!-- <b-form class="av-tooltip tooltip-label-bottom">
+        <draggable v-model="listImages">
+          <div v-for="(image, index) in listImages" :key="index">
+            <b-row no-gutters>
+              <b-col md="5">
+                <b-card class="m-3" :img-src="image.url" img-left no-body />
+              </b-col>
+              <b-col md="7">
+                <b-card-body>
+                  <b-row>
+                    <span>Upload Time :</span>
+                    <span>{{ timeFormat(image.uploadTime) }}</span>
+                  </b-row>
+                  <b-row>
+                    <!-- <span>IsMain : </span>
+                  <span><b-form-checkbox disabled v-model="image.isMain" /></span> -->
+                    <b-form-checkbox
+                      v-model="image.isMain"
+                      @change="changeMain(image.id)"
+                    >
+                      isMain
+                    </b-form-checkbox>
+                  </b-row>
+                  <b-row>
+                    <!-- <span>IsActive : </span> -->
+                    <!-- <b-form-checkbox disabled v-model="image.isActive" text-field="IsActive" /> -->
+                    <b-form-checkbox
+                      v-model="image.isActive"
+                      @change="clearAll"
+                    >
+                      IsActive
+                    </b-form-checkbox>
+                  </b-row>
+                  <!-- <b-form class="av-tooltip tooltip-label-bottom">
                   <b-form-group>
                     <b-form-checkbox v-model="isMain"> isMain </b-form-checkbox>
                     <b-form-checkbox v-model="isActive">
@@ -51,22 +80,34 @@
                     </b-form-checkbox>
                   </b-form-group>
                 </b-form> -->
+                </b-card-body>
+              </b-col>
+            </b-row>
+            <div class="d-flex flex-row-reverse">
+              <b-button
+                variant="outline-danger"
+                size="sm"
+                class="mb-2 ml-2"
+                @click="deleteMedia(image.id)"
+              >
+                <i class="simple-icon-trash"></i
+              ></b-button>
+              <!-- <b-button variant="outline-danger" size="sm" class="mb-2"  > <i class="simple-icon-trash"></i></b-button> -->
+            </div>
 
-              </b-card-body>
-            </b-col>
-          </b-row>
-          <div class="d-flex flex-row-reverse">
-            <b-button variant="outline-danger" size="sm" class="mb-2 ml-2" @click="deleteMedia(image.id)"> <i
-                class="simple-icon-trash"></i></b-button>
-            <!-- <b-button variant="outline-danger" size="sm" class="mb-2"  > <i class="simple-icon-trash"></i></b-button> -->
+            <div class="separator mb-5"></div>
           </div>
-
-          <div class="separator mb-5"></div>
-        </div>
+        </draggable>
       </div>
       <template slot="modal-footer">
-        <b-button variant="outline-secondary" @click="hideModal('editPropertyMedia')">Cancel</b-button>
-        <b-button variant="primary" @click.prevent="update()" class="mr-1">Save</b-button>
+        <b-button
+          variant="outline-secondary"
+          @click="hideModal('editPropertyMedia')"
+          >Cancel</b-button
+        >
+        <b-button variant="primary" @click.prevent="update()" class="mr-1"
+          >Save</b-button
+        >
       </template>
     </b-modal>
   </div>
@@ -76,6 +117,9 @@
 import { mapActions, mapGetters } from "vuex";
 export default {
   name: "UpdatePropertyMainFeaturesModal",
+  components: {
+    draggable: () => import("vuedraggable"),
+  },
   props: {
     item: Array,
     id: Number,
@@ -108,7 +152,6 @@ export default {
     }),
     async addMedia() {
       let formData = new FormData();
-
 
       if (this.isMain) {
         formData.append("file", this.file);
@@ -165,6 +208,9 @@ export default {
         // this.hideModal("editPropertyMedia");
       }
     },
+    update(){
+      console.log(this.listImages);
+    },
     hideModal(refname) {
       this.$refs[refname].hide();
     },
@@ -185,6 +231,13 @@ export default {
     clearAll() {
       this.files = [];
       this.file = null;
+    },
+    changeMain(id) {
+      this.listImages.forEach((image) => {
+        if (image.id != id) {
+          image.isMain = false;
+        }
+      });
     },
   },
   computed: {
